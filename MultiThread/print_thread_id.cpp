533535efae4dd64cfb41997633c47410f0ab3c62
TwoRuler <tworuler@163.com>
2015-01-11 13:18:15 +0800

    add Client.cpp Server.cpp

---
+++ MultiThread/print_thread_id.cpp
@@ -0,0 +1,34 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+pthread_t ntid;
+
+void printids(const char *s) {
+    pid_t pid;
+    pthread_t tid;
+
+    pid = getpid();
+    tid = pthread_self();
+    printf("%s pid %u tid %u (0x%x)\n", s, (unsigned int)pid,
+        (unsigned int)tid, (unsigned int)tid);
+}
+
+void * thr_fn(void *arg) {
+    printids("new thread: ");
+    return ((void *)0);
+}
+
+int main(void) {
+    int err;
+    err = pthread_create(&ntid, NULL, thr_fn, NULL);
+    if (err != 0) {
+        printf("can't create thread: %d\n", err);
+        return 1;
+    }
+    printids("main thread:");
+    sleep(1);
+    exit(0);
+}
+
